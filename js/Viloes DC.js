20fa3b66398154b273cec0120f4b265c0ea316df
JoseCarlos-13 <josecarlosbsilva.17@gmail.com>
2023-11-21 01:46:51 -0300

    create js and html folders

--- js/Viloes DC.js
+++ js/Viloes DC.js
@@ -2,38 +2,38 @@ let villainList = document.querySelector('.viloes-img');
 let villainArray = [
   {
     name: 'Arlequina',
-    profile_photo: 'imagens/viloes-perfil/arlequinapeq.jpg',
-    link: 'viloes/Arlequina.html'
+    profile_photo: '../imagens/viloes-perfil/arlequinapeq.jpg',
+    link: '../viloes/Arlequina.html'
   },
   {
     name: 'Coringa',
-    profile_photo: 'imagens/viloes-perfil/coringapeq.jpg',
-    link: 'viloes/Coringa.html'
+    profile_photo: '../imagens/viloes-perfil/coringapeq.jpg',
+    link: '../viloes/Coringa.html'
   },
   {
     name: 'Hera Venenosa',
-    profile_photo: 'imagens/viloes-perfil/hera-venenosapeq.jpg',
-    link: 'viloes/Hera Venenosa.html'
+    profile_photo: '../imagens/viloes-perfil/hera-venenosapeq.jpg',
+    link: '../viloes/Hera Venenosa.html'
   },
   {
     name: 'Charada',
-    profile_photo: 'imagens/viloes-perfil/charadapeq.jpg',
-    link: 'viloes/Charada.html'
+    profile_photo: '../imagens/viloes-perfil/charadapeq.jpg',
+    link: '../viloes/Charada.html'
   },
   {
     name: 'Pinguim',
-    profile_photo: 'imagens/viloes-perfil/pinguimpeq.jpg',
-    link: 'viloes/Pinguim.html'
+    profile_photo: '../imagens/viloes-perfil/pinguimpeq.jpg',
+    link: '../viloes/Pinguim.html'
   },
   {
     name: 'Mulher-Gato',
-    profile_photo: 'imagens/viloes-perfil/mulher-gatopeq.jpg',
-    link: 'viloes/Mulher-Gato.html'
+    profile_photo: '../imagens/viloes-perfil/mulher-gatopeq.jpg',
+    link: '../viloes/Mulher-Gato.html'
   },
   {
     name: 'Lex Luthor',
-    profile_photo: 'imagens/viloes-perfil/lex-luthorpeq.jpg',
-    link: 'viloes/Lex Luthor.html'
+    profile_photo: '../imagens/viloes-perfil/lex-luthorpeq.jpg',
+    link: '../viloes/Lex Luthor.html'
   },
 ]
 
